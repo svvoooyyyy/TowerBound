2a8788ee8e4050833bdc91d0633abc6469009c1a
Frouvie <andrewpodluzhny@gmail.com>
2023-07-21 17:40:12 +0300

    bug version
    bug with wall jump

--- Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -12,10 +12,26 @@ public class PlayerMovement : MonoBehaviour
 
     [Header("Jump")]
     [SerializeField] private float _jumpForce;
-    [SerializeField] private float _checkRadius;
+    [SerializeField] private float _checkGroundRadius;
     [SerializeField] private Transform _feetPosition;
     [SerializeField] private LayerMask _groundLayer;
     private bool _isGrounded;
+
+    [Header("Wall Jump")]
+    [SerializeField] private float _xWallForce;
+    [SerializeField] private float _yWallForce;
+    [SerializeField] private float _wallJumpingDuration;
+    private float _wallJumpingDirection;
+    private bool _isWallJumping;
+    private bool _canWallJump;
+
+    [Header("Wall Sliding")]
+    [SerializeField] private float _wallSlidingSpeed;
+    [SerializeField] private float _checkWallRadius;
+    [SerializeField] private Transform _wallCheck;
+    [SerializeField] private LayerMask _wallLayer;
+    private bool _isTouchingWall;
+    private bool _isWallSliding;
 
     private Rigidbody2D _rb;
 
@@ -24,32 +40,87 @@ public class PlayerMovement : MonoBehaviour
         _rb = GetComponent<Rigidbody2D>();
     }
 
+    private void FixedUpdate()
+    {
+        if (_isWallJumping) // Freeze the player during the duration of the wall jump
+        {
+            _horizontalMove = Input.GetAxis("Horizontal");
+            _rb.velocity = new Vector3(_horizontalMove * _speed, _rb.velocity.y, 0.0f); // Move
+
+            if (_isFacingRight && _horizontalMove > 0) // Turn left
+            {
+                Flip();
+            }
+            else if (!_isFacingRight && _horizontalMove < 0) // Turn right
+            {
+                Flip();
+            }
+        }
+    }
     private void Update()
     {
-        _isGrounded = Physics2D.OverlapCircle(_feetPosition.position, _checkRadius, _groundLayer);
+        _isGrounded = Physics2D.OverlapCircle(_feetPosition.position, _checkGroundRadius, _groundLayer); // Check ground
 
-        if (_isGrounded && Input.GetKeyDown(KeyCode.Space))
+        if (_isGrounded && Input.GetKeyDown(KeyCode.Space)) // Jump
         {
             _rb.velocity = _jumpForce * Vector3.up;
         }
+
+        WallSliding();
+        WallJump();
     }
 
-    private void FixedUpdate()
+    private void WallSliding() // Wall slide
     {
-        _horizontalMove = Input.GetAxis("Horizontal");
-        _rb.velocity = new Vector3(_horizontalMove * _speed, _rb.velocity.y);
+        _isTouchingWall = Physics2D.OverlapCircle(_wallCheck.position, _checkWallRadius, _wallLayer); // Check wall
 
-        if (_isFacingRight && _horizontalMove < 0)
+        if (_isTouchingWall && !_isGrounded && _horizontalMove != 0)
         {
-            Flip();
+            _isWallSliding = true;
+            _rb.velocity = new Vector3(_rb.velocity.x, Mathf.Clamp(_rb.velocity.y, -_wallSlidingSpeed, float.MaxValue), 0.0f);
         }
-        else  if (!_isFacingRight && _horizontalMove > 0)
+        else
         {
-            Flip();
+            _isWallSliding = false;
         }
     }
 
-    private void Flip()
+    private void WallJump() // Wall jump
+    {
+        if (_isWallSliding)
+        {
+            _isWallJumping = false;
+            _wallJumpingDirection = -transform.localScale.x;
+
+            CancelInvoke(nameof(StopWallJumping));
+        }
+
+        if (_isGrounded)
+        {
+            _canWallJump = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && _isWallSliding && _canWallJump)
+        {
+            _rb.velocity = new Vector3(_xWallForce * _wallJumpingDirection, _yWallForce, 0.0f);
+            _isWallJumping = true;
+            _canWallJump = false;
+
+            if (transform.localScale.x != _wallJumpingDirection)
+            {
+                Flip();
+            }
+
+            Invoke(nameof(StopWallJumping), _wallJumpingDuration);
+        }
+    }
+
+    private void StopWallJumping()  
+    {
+        _isWallJumping = false;
+    }
+
+    private void Flip() // Turn in the opposite direction
     {
         _isFacingRight = !_isFacingRight;
         Vector3 Scaler = transform.localScale;
