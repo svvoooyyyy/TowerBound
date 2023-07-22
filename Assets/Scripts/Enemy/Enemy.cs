7f63deb3038857ac832df3d109f796df0f22568b
banana-dan <glokback@gmail.com>
2023-07-23 01:02:23 +0500

    enemy ai added)
    bat added
    sprites added

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -2,14 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private int _maxHealth;
+    [SerializeField] protected float _moveSpeed;
+    [SerializeField] private GameObject _target;
+    private Rigidbody2D _rb;
     private int _currentHealth;
+
 
     private void Start()
     {
+        _rb = GetComponent<Rigidbody2D>();
         _currentHealth = _maxHealth;
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        _rb.AddForce((_target.transform.position - transform.position).normalized * _moveSpeed * Time.fixedDeltaTime, ForceMode2D.Force);
     }
 
     public void TakeDamage(int damage)
@@ -26,4 +42,6 @@ public class Enemy : MonoBehaviour
     {
         
     }
+
+    
 }
