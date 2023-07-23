d028cd5cb275dc268d06ff703652b7a081d12403
banana-dan <glokback@gmail.com>
2023-07-23 12:17:53 +0500

    sword animation added

--- Assets/Scripts/Player/Fight/PlayerCombat.cs
+++ Assets/Scripts/Player/Fight/PlayerCombat.cs
@@ -8,6 +8,15 @@ public class PlayerCombat : MonoBehaviour
     [SerializeField] private float _attackRange;
     [SerializeField] private Transform _attackPoint;
     [SerializeField] private LayerMask _enemyLayer;
+    private Rigidbody2D _rb;
+
+    [Header("sword animation")]
+    [SerializeField] private Animator _sword;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
 
     private void Update()
     {
@@ -15,16 +24,20 @@ public class PlayerCombat : MonoBehaviour
         {
             Attack();
         }
+        _sword.SetFloat("jump velocity", _rb.velocity.y);
+
     }
 
     private void Attack()
     {
+        _sword.SetTrigger("attack");
         Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(_attackPoint.position, _attackRange, _enemyLayer);
         
         foreach (Collider2D enemy in hitEnemies)
         {
             enemy.GetComponent<Enemy>().TakeDamage(_attackDamage);
         }
+        
     }
 
     private void OnDrawGizmosSelected()
